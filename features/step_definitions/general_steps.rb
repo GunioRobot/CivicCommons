7b1ff822c24a449f88bfe846a29bfd59cab3189b
Parama Danoesubroto <paramaw@gmail.com>
2011-02-28 12:11:12 -0500

    8457517 link local account with facebook. temporary commit.

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -2,6 +2,17 @@ Given /^I am on the (\w+) index page/ do |resource|
   visit send("#{resource.downcase.pluralize}_url")
 end
 
+Given /^I have a confirmed local user account$/ do
+  @current_person = Factory.create(:registered_user)
+end
+
+Given /^I am logged in$/ do
+  visit '/people/login'
+  fill_in 'person[email]', with: @current_person.email
+  fill_in 'person[password]', with: 'password'
+  click_button 'person_submit'
+end
+
 Then /^I should receive an? "([^"]*)" with the message:$/ do |error, string|
   @code_to_run.should raise_error(error.constantize, string)
 end
