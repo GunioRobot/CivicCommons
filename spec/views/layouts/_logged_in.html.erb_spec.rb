726f56e667cb284d63feedc2c64fe476c103f19a
Parama Danoesubroto <paramaw@gmail.com>
2011-03-08 23:09:00 -0500

    Refactor spec for _logged_in.html.erb

--- spec/views/layouts/_logged_in.html.erb_spec.rb
+++ spec/views/layouts/_logged_in.html.erb_spec.rb
@@ -1,17 +1,29 @@
 require 'view_spec_helper'
+require 'devise/test_helpers'
 
 describe "layouts/_logged_in.html.erb" do
+  
   before(:each) do
-    # @person = stub_model(Person,
-    #   :facebook_authenticated? => true
-    # )
+    @person = stub_model(Person,
+      :facebook_authenticated? => true
+    )
+    @view.stub(:resource).and_return(@person)
+    @view.stub(:resource_name).and_return('person')
+    @view.stub(:devise_mapping).and_return(Devise.mappings[:person])
+    @view.stub(:current_person).and_return(@person)  
+    @view.should_receive(:user_profile).and_return('')
+    @view.should_receive(:text_profile).and_return('')
+    @view.should_receive(:link_to_settings).and_return('')
+    # @view.stub_chain(:user_profile,:html_safe).and_return('')
+    # @view.stub_chain(:text_profile,:html_safe).and_return('')
   end
 
-  it "renders the facebook link when a person don't have a facebook account" do
-    Webrat.class.should == ''
+  it "should have logout link" do
+    # Webrat.inspect.should == ''
     render
-    rendered.should have_content('a.hello')
-    # rendered.should contain('hello')
-    # response.should have_text "hello"
+    rendered.should contain 'Logout'
+    # rendered.assert_have_selector 'a.user-link'
+    # rendered.have_tag('a.user-link').should be_true
+    # rendered.should have_tag 'a'
   end
 end
