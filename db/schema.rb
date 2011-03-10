67132bc7ac464e662683ebddd505ed401b04206a
Parama Danoesubroto <paramaw@gmail.com>
2011-03-09 20:51:42 -0500

    after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110223184348) do
+ActiveRecord::Schema.define(:version => 20110301062718) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -175,7 +175,9 @@ ActiveRecord::Schema.define(:version => 20110223184348) do
     t.string   "password"
     t.string   "email",                               :default => "",    :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
+    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
