d19fb5c667ca1cffd9cfe39ed5d32ada90b48545
Parama Danoesubroto <paramaw@gmail.com>
2011-03-09 23:19:32 -0500

    added migration for declined_fb_auth col

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110301062718) do
+ActiveRecord::Schema.define(:version => 20110310021638) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -202,6 +202,7 @@ ActiveRecord::Schema.define(:version => 20110301062718) do
     t.datetime "marketable_at"
     t.text     "bio"
     t.boolean  "daily_digest",                        :default => true,  :null => false
+    t.boolean  "declined_fb_auth"
   end
 
   add_index "people", ["email"], :name => "index_people_on_email", :unique => true
